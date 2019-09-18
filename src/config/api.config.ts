3aa1a0c12fa7bdab8e5bd4009259a202089b531c
iabdullaiev <iabdullaiev@castlighthealth.com>
2019-09-18 09:37:20 +0300

    Update API keys.

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,10 +1,10 @@
 export const ApiConfig = {
   OpenWeather: {
     API: 'https://api.openweathermap.org/',
-    APP_ID: '004cf397255b0b492b39dc329bbba249'
+    APP_ID: 'b1b9e3085df5a0cdfbe91c583a8da2ef'
   },
   GoogleMaps: {
     API: 'https://maps.googleapis.com/maps/api/',
-    API_KEY: 'AIzaSyDGUXaq4u0eqBhn6zb-_lVotmokT-a9q1Y'
+    API_KEY: 'AIzaSyA2O_vqmii8QkF_STLCkgigXwGvfH6Zy2c'
   }
 };
